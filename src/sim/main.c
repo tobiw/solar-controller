bbb4675b4fef834e9364e351e9088056183b673a
Tobi Wulff <tobi@swulff.de>
2020-05-12 16:07:00 +1200

    sim: read sensor inputs from text file

--- src/sim/main.c
+++ src/sim/main.c
@@ -4,12 +4,37 @@
 
 sim_state_t g_sim_state;
 
+static int read_inputs_file(char *buf)
+{
+    FILE *fp = NULL;
+    if ((fp = fopen("inputs.txt", "r")) != NULL)
+    {
+        const int len = fread(buf, 1, 512, fp);
+        fclose(fp);
+        return len;
+    }
+    return -1;
+}
+
 static void sim_update_inputs(sim_state_t *s)
 {
-    s->temp_sensors[0] = 400;
-    s->temp_sensors[1] = 400;
-    s->temp_sensors[2] = 400;
-    s->temp_sensors[3] = 400;
+    char buf[512] = {0};
+    const int r = read_inputs_file(buf);
+
+    if (r > 0)
+    {
+        int r = sscanf(buf, "%d,%d,%d,%d",
+                       &s->temp_sensors[0], &s->temp_sensors[1], &s->temp_sensors[2], &s->temp_sensors[3]);
+        printf("sscanf %d\n", r);
+    }
+    else
+    {
+        // Use default values if file is not present
+        s->temp_sensors[0] = 400;
+        s->temp_sensors[1] = 400;
+        s->temp_sensors[2] = 400;
+        s->temp_sensors[3] = 400;
+    }
 }
 
 static void sim_update_ui(sim_state_t *s)
